f0f05d2cbcf6da3d9be00ffd6348bf220d3a6d7c
Lahis <lahis_gomes@hotmail.com>
2015-04-16 23:05:02 -0400

    teste

--- Assets/Source Code/Project/Players/PlayerArcher.cs
+++ Assets/Source Code/Project/Players/PlayerArcher.cs
@@ -4,14 +4,28 @@ using System.Collections;
 public class PlayerArcher : GenericPlayer
 {
 
+    private Timer _canAttack;
+
+    void Awake()
+    {
+        _speed = 20f;
+        _projectile = 2;
+        _canAttack = new Timer(0.5f);
+    }
+
     public override string Action1(float time, int[] direction)
     {
-        return "Attack";
+        if (_canAttack.GetTime() > 0.5f)
+        {
+            _facadePlayer.SpawProjectile(new Vector2(direction[0], direction[1]));
+            _canAttack.Reset();
+            return "Attack";
+        }
+        return null;
     }
 
     public override string Action2(float time, int[] direction)
     {
-
         return "ss";
     }
 
