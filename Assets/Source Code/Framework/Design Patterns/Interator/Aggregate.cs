9ebc80d25a85b7b0442271b5b847019842b3c6ff
Jackson Daniel <jackson.daniel.17dh@gmail.com>
2015-05-13 19:21:43 -0300

    apaguei pra ajeitar

--- Assets/Source Code/Framework/Design Patterns/Interator/Aggregate.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Aggregate 
-{
-	public abstract Interator CreateInterator();
-    public abstract Vector3 Get(int index);
-    public abstract int       Count();
-}
--- Assets/Source Code/Project/Effects/GenericEffect.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class GenericEffect
-{
-    public abstract void BornEffect();
-    public abstract void LivingEffect();
-    public abstract void DieEffect();
-}
