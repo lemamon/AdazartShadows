30c76790d21d41dfa4fec348a85ed47c4ccc53fd
Lahis <lahis_gomes@hotmail.com>
2015-05-14 17:19:23 -0400

    TelasMenu

---
+++ Assets/Lahis/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public float time;
+    public string scene;
+    private bool flag;
+    
+	void Start () {
+        flag = false;
+        if (Application.loadedLevelName == "LudusSplash")
+        {
+            Invoke("GoToScene", time);
+        }
+        else
+            flag = true;
+       	
+	}
+
+    void Update()
+    {
+        if(flag && Input.anyKey)
+            GoToScene();
+    }
+
+	void GoToScene () {
+        Application.LoadLevel(scene);
+	}
+
+    
+}
